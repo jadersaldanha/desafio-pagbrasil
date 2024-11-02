2903e5d97acc75590d6c8387aad0f1f8e42c202a
jadersaldanha <saldanha.jader@gmail.com>
2024-11-02 11:05:18 -0300

    adicionando cenárioas

--- cypress/e2e/cucumber/feature/contactForm.feature
+++ cypress/e2e/cucumber/feature/contactForm.feature
@@ -1,4 +1,16 @@
-Feature: Formulario de contato
+Feature: Formulario de contato para usuários da PagBrasil
 
 Scenario: Preencher Formulario Corretamente
 Given Usuario está na pagina do Formulario
+And Preenche nome, email, empresa, website, phone e inquiry
+Then A mensagem de sucesso é exibida
+
+Scenario: Preencher Formulario com dados incorretos
+Given Usuario está na pagina do Formulario
+And Preenche nome, email, empresa, website, phone e inquiry com dados incorretos
+Then Valida a exibição da mensagem de campo inválido
+
+Scenario: Tenta preencher formulario sem dados
+Given Usuario está na pagina do Formulario
+And Clica em submit sem preencher dados
+Then Valida a exibição da mensagem do tooltip
--- cypress/e2e/cucumber/stepDefinitions/contactForm/contactForm_stepdef.js
+++ cypress/e2e/cucumber/stepDefinitions/contactForm/contactForm_stepdef.js
@@ -2,10 +2,14 @@ import {Given, When, Then} from "cypress-cucumber-preprocessor/steps"
 
 beforeEach(function(){
     cy.fixture('contactFormData').as('data')
+    cy.fixture('contactFormInvalidData').as('InvalidData')
 })
 
 Given('Usuario está na pagina do Formulario',function(){
     cy.visit('https://www.pagbrasil.com/fb831484f9611648f05411b3746466b6/form.html')
+})
+
+Given('Preenche nome, email, empresa, website, phone e inquiry', function(){
     cy.get('#name').type(this.data.name)
     cy.get('#email').type(this.data.email)
     cy.get('#company').type(this.data.company)
@@ -13,5 +17,39 @@ Given('Usuario está na pagina do Formulario',function(){
     cy.get('#phone').type(this.data.phone)
     cy.get('#inquiry').type(this.data.inquiry)
     cy.get('button').click()
-    cy.get('#successMessage')
+})
+
+Given('A mensagem de sucesso é exibida', function(){
+    cy.get('#successMessage').should('be.visible')
+})
+
+Given('Preenche nome, email, empresa, website, phone e inquiry com dados incorretos', function(){
+    cy.get('#name').type(this.InvalidData.name)
+    cy.get('#email').type(this.InvalidData.email)
+    cy.get('#company').type(this.InvalidData.company)
+    cy.get('#website').type(this.InvalidData.website)
+    cy.get('#phone').type(this.InvalidData.phone)
+    cy.get('#inquiry').type(this.InvalidData.inquiry)
+    cy.get('button').click()
+})
+
+Given('Valida a exibição da mensagem de campo inválido', function(){
+    cy.contains('error', 'Campo nome inválido').should('be.visible');
+    cy.contains('error', 'Campo email inválido').should('be.visible');
+    cy.contains('error', 'Campo email inválido').should('be.visible');
+    cy.contains('error', 'Campo company inválido').should('be.visible');
+    cy.contains('error', 'Campo website inválido').should('be.visible');
+    cy.contains('error', 'Campo phone inválido').should('be.visible');
+    cy.contains('error', 'inquiry').should('be.visible');
+})
+
+Given('Clica em submit sem preencher dados', function(){
+    cy.get('button').click()
+})
+
+Given('Valida a exibição da mensagem do tooltip', function(){
+    cy.get('#email').then(($el) => {
+        // A propriedade 'validationMessage' deve conter a mensagem de erro
+        cy.wrap($el[0].validationMessage).should('contain', 'Preencha este campo.');
+    });
 })
